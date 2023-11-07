7e62d7625d538459cd29a93fccc1c0ed36adccf9
ANAMIKA JAIN <78084670+ANAMIKA1410@users.noreply.github.com>
2023-11-07 16:02:16 +0530

    Create 07 novemember.cpp

---
+++ coding days/07 novemember.cpp
@@ -0,0 +1,105 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<string>
+using namespace std;
+int main() 
+{
+    
+    // for(int i=10;i>=1;i--)
+    // {
+    //     cout<<i<<" ";
+    // }
+    // cout<<endl;
+    // for(int i=1;i<=10;i++)
+    // {
+    //     cout<<i<<" ";
+    // }
+    
+    
+    
+    
+    // int num;
+    // cin>>num;
+    // int i=1;
+    // while(i<11)
+    // {
+    //      cout<<num<<" * "<<i<<" = "<<num*i<<endl;
+    //      i++;
+    // }
+    
+    
+    // int i=5;
+    // while(i<=100)
+    // {
+    //     cout<<i<<" ";
+    //     i+=5;
+    // }
+    
+    
+    
+    
+    
+    // int num;
+    // cin>>num;
+    // 2 * 1 = 2
+    // 2 * 7 = 2*7
+    // for(int i=1;i<=10;i++)
+    // {
+    //     cout<<num<<" * "<<i<<" = "<<num*i<<endl;
+    // }
+    
+    // for(int i=5;i<=100;i+=5)
+    // {
+    //     cout<<i<<" ";
+    // }
+    // int marks=80;
+    // cout<<"marks are:"<<marks<<endl;
+    // for(int i=1;i<=10;i++)
+    // {
+        
+    //     cout<<"the value of i this time is :"<<i<<endl;
+    // }
+  //  Hello and welcome here
+   
+//  What are the loops in C++?
+// Loops in C++ | Different Types of Loops in C++ with Examples
+// Loop statements in C++ execute a certain block of the code or statement
+// multiple times, mainly used to reduce the length of the code by executing the 
+// same function multiple times and reducing the code's redundancy. 
+// for-- range predefined
+// while -- condition
+// do while -- same as while loop but it execute atleast once
+   
+   
+   
+   
+   
+    // int age;
+    // cout<<"Enter your age :";
+    // cin>>age;
+    
+    // if(age>=0 && age<18)
+    // {
+    //     cout<<"You are not eligible for voting \n";
+    // }
+    // else if(age>=18 && age<=110)
+    // {
+    //     cout<<"You are eligible for voting \n";
+    // }
+    // else
+    // {
+    //     cout<<"Your age is invalid";
+    // }
+    
+    
+ 
+  
+    
+    
+// 0-18
+// age>18
+// age not valid 110 
+    
+
+    return 0;
+}
