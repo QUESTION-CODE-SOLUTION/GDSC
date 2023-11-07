5979650041e204ed38ff97bf6494081ed46ed7f0
devanshg13 <139626097+devanshg13@users.noreply.github.com>
2023-11-07 16:14:02 +0530

    Create Devansh_Gupta.cpp

---
+++ Summit your code/Devansh_Gupta.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<string>
+using namespace std;
+int main() 
+{
+    
+    for(int i=10;i>=1;i--)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    for(int i=1;i<=10;i++)
+    {
+        cout<<i<<" ";
+    }
+    
+}
